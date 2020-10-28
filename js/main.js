abd4a5ee14b88585c448ddf99de35d06bb0b73e3
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-28 17:00:50 +0100

    Javascript OK

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('ook js');
+//  Far apparire il menu al click 
+
+$(document).ready( function () {
+
+  var dropLink = $('.with-dropdown > a');
+  var dropMenu = $('.with-dropdown > .menu-dropdown')
+
+  dropLink.click(function () {
+
+    var activeMenu = $(this).next('.menu-dropdown');
+
+    dropMenu.not(activeMenu).hide();
+    activeMenu.toggle();
+
+  })
+
+});
